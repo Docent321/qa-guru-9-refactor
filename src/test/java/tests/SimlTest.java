52754e1ac108001ca8a2034b684451c14c6ac8d5
Alexandr Donskov <mrad@list.ru>
2022-03-01 23:00:53 +0300

    add test

---
+++ src/test/java/tests/SimlTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SimlTest {
+    @Test
+    void selenideSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("selenide.org"));
+    }
+}
