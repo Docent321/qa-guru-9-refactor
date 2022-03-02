257315170bbc9fb624f9efb6fad715f3c3111e1b
Artem Kidalov <96776061+a6r12@users.noreply.github.com>
2022-03-02 21:48:42 +0300

    add assertFalse test (#2)

--- src/test/java/tests/SimlTest.java
+++ src/test/java/tests/SimlTest.java
@@ -2,11 +2,13 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -21,6 +23,12 @@ public class SimlTest {
     @Test
     void assertTrueTest() {
         assertTrue(3 > 2);
+    }
+
+    @Test
+    @DisplayName("AssertFalse")
+    void assertFalseTest() {
+        assertFalse(3 < 2);
     }
 
 }
